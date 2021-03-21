a45af91ed64b56c4a24d865964f5b78a60e49277
Vito <topvetal@gmail.com>
2021-03-21 23:41:22 +0200

    class

--- task-3.js
+++ task-3.js
@@ -5,54 +5,63 @@
 // Плагин ожидает следующую HTML-разметку и показывает четыре цифры: дни, часы, минуты и секунды в формате XX:XX:XX:XX.
 // Количество дней может состоять из более чем двух цифр.
 
-$(".date").datetimepicker({
-  format: "YYYY-MM-DD HH:mm",
-  defaultDate: new Date(),
-  locale: "ru",
-  icons: {
-    time: "glyphicon glyphicon-time",
-    date: "glyphicon glyphicon-calendar",
-    up: "glyphicon glyphicon-chevron-up",
-    down: "glyphicon glyphicon-chevron-down",
-  },
-});
-let inputDate = $(".form-control")
-let promotion = new Date(inputDate.val());
-let timer = $("#timer-1");
-let calcButton = $('#calc')
+$(document).ready(() => {
+  class Timer {
+    constructor(data) {
+      this.promotion = Date.now();
+      this.timer = $("#timer-1");
+      this.calcButton = $("#calc");
+      this.id = setInterval(() => this.tm(this.promotion), 1000);
+      this.datepicker();
+      this.isClick();
+      console.log(this.promotion);
+    }
+    
+    datepicker() {
+      $(".date").datetimepicker({
+        format: "YYYY-MM-DD HH:mm",
+        defaultDate: new Date(),
+        locale: "ru",
+        icons: {
+          time: "glyphicon glyphicon-time",
+          date: "glyphicon glyphicon-calendar",
+          up: "glyphicon glyphicon-chevron-up",
+          down: "glyphicon glyphicon-chevron-down",
+        },
+      });
+    }
 
-
-let id
-id = setInterval(() => tm(promotion), 1000);
-
-function tm(promDate) {
-  let
-    now = new Date(),
-    distance = promDate - now,
-    days = Math.floor(distance / (1000 * 60 * 60 * 24)),
-    hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)),
-    minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60)),
-    seconds = Math.floor((distance % (1000 * 60)) / 1000);
-  if (promDate !== $(".form-control").val()) calcButton.removeAttr('disabled')
-  if (promDate < now) {
-    timer.html(`<div class="time-is-over">Time is OVER</div>`)
-    id = clearInterval(id)
-    return
-  }
-  timer.html(`<div class="field">
+    tm(promDate) {
+      let now = new Date(),
+        distance = promDate - now,
+        days = Math.floor(distance / (1000 * 60 * 60 * 24)),
+        hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)),
+        minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60)),
+        seconds = Math.floor((distance % (1000 * 60)) / 1000);
+      if (promDate !== $(".form-control").val())
+        this.calcButton.removeAttr("disabled");
+      if (promDate < now) {
+        this.timer.html(`<div class="time-is-over">Time is OVER</div>`);
+        this.id = clearInterval(this.id);
+        return;
+      }
+      this.timer.html(`<div class="field">
         <p class="value" data-value="days"><span>${days}</span> days</p>
         <p class="value" data-value="hours"><span>${hours}</span> hours</p>
-        <p class="value" data-value="mins"><span>${minutes}</span> hours</p>
+        <p class="value" data-value="mins"><span>${minutes}</span> minutes</p>
         <p class="value" data-value="secs"><span>${seconds}</span> seconsds</p>
-      </div>`)
-  
-}
+      </div>`);
+    }
 
-calcButton.on('click', evt => {
-  $(evt.target).attr('disabled', 'disabled')
-  let newDate = $(".form-control").val()
-  clearInterval(id)
-  id = setInterval(() => tm(new Date(newDate)), 1000);
-})
-
-
+    isClick() {
+      this.calcButton.on("click", (evt) => {
+        $(evt.target).attr("disabled", "disabled");
+        let newDate = $(".form-control").val();
+        clearInterval(this.id);
+        this.id = setInterval(() => this.tm(new Date(newDate)), 1000);
+      });
+    }
+  }
+  let inputDate = $(".form-control").val();
+  let timer = new Timer(inputDate);
+});
